140fdd8370627f2b31d9e9e5b3ff5f9b5113ec30
tterb <bstevensondev@gmail.com>
2018-12-28 16:23:45 -0800

    :calendar: Add day #21 solution

---
+++ day21.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+oper = {
+    'addr': lambda reg, a, b: reg[a] + reg[b],
+    'addi': lambda reg, a, b: reg[a] + b,
+    'mulr': lambda reg, a, b: reg[a] * reg[b],
+    'muli': lambda reg, a, b: reg[a] * b,
+    'banr': lambda reg, a, b: reg[a] & reg[b],
+    'bani': lambda reg, a, b: reg[a] & b,
+    'borr': lambda reg, a, b: reg[a] | reg[b],
+    'bori': lambda reg, a, b: reg[a] | b,
+    'setr': lambda reg, a, b: reg[a],
+    'seti': lambda reg, a, b: a,
+    'gtir': lambda reg, a, b: 1 if a > reg[b] else 0,
+    'gtri': lambda reg, a, b: 1 if reg[a] > b else 0,
+    'gtrr': lambda reg, a, b: 1 if reg[a] > reg[b] else 0,
+    'eqir': lambda reg, a, b: 1 if a == reg[b] else 0,
+    'eqri': lambda reg, a, b: 1 if reg[a] == b else 0,
+    'eqrr': lambda reg, a, b: 1 if reg[a] == reg[b] else 0,
+}
+
+inp = [i.strip() for i in stdin.readlines()]
+ip = int(inp.pop(0).split()[1])
+inst = [[i.split()[0],int(i.split()[1]),int(i.split()[2]),int(i.split()[3])] for i in inp]
+reg = [0,0,0,0,0,0]
+n = reg[ip]
+while n < len(inst):
+    op, a, b, c = inst[n]
+    reg[c] = oper[op](reg, a, b)
+    # print(f'{n}: ({op} {a} {b} {c}): {reg}')
+    reg[ip] += 1
+    n = reg[ip]
+    if n == 29:
+        print(reg[5])
+        break
