9f8a57b4e5de5541666cfa42ca9e671301d7416b
tterb <bstevensondev@gmail.com>
2018-12-30 00:54:12 -0800

    :calendar: Add day #5 pt2 solution

--- day5.py
+++ day5.py
@@ -1,17 +1,31 @@
 # --- Day 5: Alchemical Reduction ---
 
-inp = list(input().strip())
-i = len(inp)-1
-while(i > 0):
-  if abs(ord(inp[i])-ord(inp[i-1])) == 32:
-    inp.pop(i)
-    inp.pop(i-1)
-    if i < len(inp)-2:
-      i += 2
-    else:
-      i = len(inp)-1
-  i -= 1
-if abs(ord(inp[len(inp)-2])-ord(inp[len(inp)-1])) == 32:
-  inp = inp[:-2]
-print(''.join(inp))
-print(len(inp))
+def react(poly):
+  i = len(poly)-1
+  while(i > 0):
+    if abs(ord(poly[i])-ord(poly[i-1])) == 32:
+      poly.pop(i)
+      poly.pop(i-1)
+      if i < len(poly)-2:
+        i += 2
+      else:
+        i = len(poly)-1
+    i -= 1
+  if abs(ord(poly[len(poly)-2])-ord(poly[len(poly)-1])) == 32:
+    poly = poly[:-2]
+  return poly
+
+# poly = list(input().strip())
+poly = react(list(input().strip()))
+print(f'Part 1: {len(poly)}')
+minVal = len(poly)
+for i in range(65, 91):
+  mod = list(filter((chr(i+32)).__ne__, list(filter((chr(i)).__ne__, poly))))
+  val = len(react(mod))
+  if val < minVal:
+    minVal = val
+print(f'Part 2: {minVal}')
+    
+    
+  
+  
